d47b6fce8b72d7ae6968b2e6a8aa4446c2ce40f0
Ortiz1124X <124809800+Ortiz1124X@users.noreply.github.com>
2023-04-25 17:42:46 +0200

    Add files via upload

---
+++ Eje 2.js
@@ -0,0 +1,15 @@
+
+const list = ["Luis Felipe Hernandez Ortiz", 19, true, ];
+console.log(list)
+
+const fecha_cadena = new Date ("June 16 2003")
+console.log(fecha_cadena)
+
+const Libro = {
+    titulo: "Satanas",
+    autor: "Mario Mendoza",
+    fecha: 2002,
+    url: ('https://www.casadellibro.com/libro-satanas/9789584273543/11183783'),
+}
+
+console.log(Libro)
