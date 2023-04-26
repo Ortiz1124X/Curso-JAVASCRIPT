9045ff8729580368b8b9ec65e602123c51c424db
Ortiz1124X <124809800+Ortiz1124X@users.noreply.github.com>
2023-04-26 16:08:58 +0200

    Add files via upload

---
+++ Eje 5.js
@@ -0,0 +1,38 @@
+// - Una variable que contenga tu altura en centímetros (entero)
+
+let altura = 175;
+console.log(altura);
+
+// - Una variable que contenga tu altura en metros (número de coma flotante)
+
+let altura_fl = 1.75;
+console.log(altura_fl);
+
+// - Una variable que contenga tu peso en kilogramos (número de coma flotante)
+
+let peso_fl = 80.4;
+console.log(peso_fl);
+
+// - Una variable que contenga tu altura en metros redondeada hacia arriba
+
+let altura_1 = 1.75;
+console.log(altura_1.toFixed(1.75))
+
+
+// - Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+
+let peso_1 = 77.9;
+console.log(Math.floor(peso_1));
+
+
+// - Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1"
+
+let max_valor = Number.MAX_VALUE;
+console.log(max_valor + 1);
+
+// es igual al máximo valor que se puede obtener en Javascript
+
+let max_valor_1 = Number.MAX_VALUE;
+console.log(max_valor_1)
+
+// Es igual al máximo valor que se puede obtener en Javascript.
